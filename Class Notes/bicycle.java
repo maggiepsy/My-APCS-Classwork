fa63081286ce31636b9f4609a7e5a34aca2ad0a2
maggiepsy <maggiepu0920@gmail.com>
2018-02-28 08:38:04 +0800

    *

--- Class Notes/bicycle.java
+++ Class Notes/bicycle.java
@@ -1,19 +1,18 @@
 /*
  * Author: Maggie Pu
-Instructor: Mr.Daniel;
-Description: This is a class written for the bicycle. 
+   Instructor: Mr.Daniel
+   Description: This is a class written for the bicycle. 
  */
+
 package classclasswork;
 import java.util.*;
 
-/**
- *
- * @author MaggiePu
- */
+
 public class bicycle {
  private int numbike;
  private int numyears;
  private int speed;
+ private String brand;
  private Random random;
  
 
@@ -39,16 +38,27 @@ public void setyears(int n){
     numyears=n;
 }
 
+public void brandname(String n){
+    brand=n;
+}
+
 public int getspeed(){
   int bikespeed=(random.nextInt(speed)+1)*15;
   return bikespeed;
 }
 
-public int getdepreciation(){
+public int depreciationcost(){
    int depreciation=0;
-   for (int i=1;i<numbike;i++)
+   
+   for (int i=1;i<numbike;i++){
        depreciation+=numbike*(random.nextInt(numyears)+1)*20;
+   }
+   
    return depreciation;
+}
+
+public String getbrandname(){
+    return brand;
 }
 
 }
