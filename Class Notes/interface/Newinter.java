924bf9c69cd4a150a3e3118b998936e4b5365c76
maggiepsy <maggiepu0920@gmail.com>
2018-03-15 09:27:38 +0800

    ~

--- Class Notes/interface/Newinter.java
+++ Class Notes/interface/Newinter.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Author: Maggie Pu
+Date: 2018.3.14
+Instructor:Mr.Daniel
+Description: This is a program designed to override the compare method to compare the area of two newly build circle. 
  */
 package newinter;
 
